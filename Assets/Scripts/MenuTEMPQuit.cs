9d7823dbfeb4939c9dc89c1f3e5c266bcc0d1cb9
TheQ-Lab <73586510+TheQ-Lab@users.noreply.github.com>
2020-12-09 22:32:43 +0100

    Material Fixes + Temp Quit-Button

---
+++ Assets/Scripts/MenuTEMPQuit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTEMPQuit : MonoBehaviour
+{
+    public void OnClickQuit()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+}
