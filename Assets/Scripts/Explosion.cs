d3c5fb0721ab6e7d946932b62fb053c5629176fd
OlgaF <o170385@yandex.com>
2021-01-12 12:07:32 +0100

    fix bug: to destroy explosion effect after explosion

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float lifetime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(destroyExplosion());
+        
+    }
+
+    IEnumerator destroyExplosion()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
