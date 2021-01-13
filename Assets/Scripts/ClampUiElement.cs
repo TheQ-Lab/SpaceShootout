b8d4a6f25fa14be922b4cfb7879141da73e100db
TheQ-Lab <73586510+TheQ-Lab@users.noreply.github.com>
2021-01-13 20:12:17 +0100

    Automate Astronaut Clamped UI Elements assignment

--- Assets/Scripts/ClampUiElement.cs
+++ Assets/Scripts/ClampUiElement.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class ClampUiElement : MonoBehaviour
 {
+    [Tooltip("Should Autofill; Is Object to track")]
     public GameObject uiElement;
     public UITypeEnum UIType;
 
-    public enum UITypeEnum {IsActive_Dependant, LifeBar, ActiveIndicator};
+    public enum UITypeEnum {IsActive_Dependant, LifeBar, ActiveIndicator, ShotText, ShotBar};
 
     private Astronaut parent;
 
     // Start is called before the first frame update
     void Start()
     {
+        parent = GetComponentInParent<Astronaut>();
+
         if (UIType == UITypeEnum.LifeBar)
         {
-            parent = GetComponentInParent<Astronaut>();
             char team = '\0';
             switch (parent.TeamNo)
             {
@@ -36,16 +38,24 @@ public class ClampUiElement : MonoBehaviour
             char astronaut = parent.AstronautNo.ToString()[0];
             uiElement = GameObject.Find("LifeBar" + team + astronaut);
         }
-        else
+        else if (UIType == UITypeEnum.ActiveIndicator)
         {
-            parent = GetComponentInParent<Astronaut>();
+            uiElement = CoolFunctions.FindInArray("ActiveIndicatorContainer", GameObject.FindGameObjectsWithTag("UIReferences"));
+        }
+        else if (UIType == UITypeEnum.ShotText)
+        {
+            uiElement = CoolFunctions.FindInArray("ShotTextContainer", GameObject.FindGameObjectsWithTag("UIReferences"));
+        }
+        else if (UIType == UITypeEnum.ShotBar)
+        {
+            uiElement = CoolFunctions.FindInArray("ShotBarContainer", GameObject.FindGameObjectsWithTag("UIReferences")).transform.GetChild(0).gameObject;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (UIType == UITypeEnum.IsActive_Dependant)
+        if (UIType == UITypeEnum.IsActive_Dependant || UIType == UITypeEnum.ShotText || UIType == UITypeEnum.ShotBar)
         {
             if (!parent.isActive) { return; }
         }
